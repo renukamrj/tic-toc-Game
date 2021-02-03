ec9694e8ff5cfecba67c3a93fd8625a622f2020c
Renuka Maharjan <renuka.maharjan@live.lagcc.cuny.edu>
2021-02-02 20:51:31 -0500

    Final tic toc

---
+++ tictok/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace tictok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(119, 0);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(120, 120);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(238, 0);
+            this.btn3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(120, 120);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(0, 119);
+            this.btn4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(120, 120);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(119, 119);
+            this.btn5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(120, 120);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(238, 119);
+            this.btn6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(120, 120);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(0, 238);
+            this.btn7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(120, 120);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(119, 238);
+            this.btn8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(120, 120);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(238, 238);
+            this.btn9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(120, 120);
+            this.btn9.TabIndex = 0;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(0, 0);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(120, 120);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(329, 537);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(262, 78);
+            this.btnNew.TabIndex = 10;
+            this.btnNew.Text = "New Game";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.ForeColor = System.Drawing.Color.Green;
+            this.lblX.Location = new System.Drawing.Point(38, 316);
+            this.lblX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(356, 63);
+            this.lblX.TabIndex = 11;
+            this.lblX.Text = "X Player Turn";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblO.ForeColor = System.Drawing.Color.Blue;
+            this.lblO.Location = new System.Drawing.Point(651, 316);
+            this.lblO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(362, 63);
+            this.lblO.TabIndex = 12;
+            this.lblO.Text = "O Player Turn";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Location = new System.Drawing.Point(284, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(1);
+            this.panel1.Size = new System.Drawing.Size(360, 360);
+            this.panel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(136, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1061, 120);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "O and  X Game Apps";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 653);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(433, 37);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Renuka Maharjan(23599681)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 693);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(511, 37);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "MAC109 C# Project(Tic Toc Game";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 730);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(448, 37);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Laguardia Community College";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 762);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnNew);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X and O Game";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ tictok/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int turn = 0;
+        private Button btn;
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btn = btn1;
+            markxo();
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btn = btn2;
+            markxo();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btn = btn3;
+            markxo();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            btn = btn4;
+            markxo();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            btn = btn5;
+            markxo();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            btn = btn6;
+            markxo();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btn = btn7;
+            markxo();
+        }
+        
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            btn = btn8;
+            markxo();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            btn = btn9;
+            markxo();
+        }
+
+
+        void markxo()
+        {
+
+            if (turn == 0 && btn.Text == "")
+            {
+               
+                this.btn.ForeColor = Color.Green;
+                this.btn.Text = "X";
+                turn = 1;
+                lblX.Text = "";
+                lblO.Text = "O Player Turn";
+                winners();
+
+            }
+            else if (turn == 1 && btn.Text == "")
+            {
+             
+                this.btn.ForeColor=Color.Blue;
+                this.btn.Text = "O";
+                turn = 0;
+                lblO.Text = "";
+                lblX.Text = "X Player Turn";
+                winners();
+               
+            }
+        }
+        
+        void winners()
+        {
+            if ((btn1.Text == "X" && btn2.Text == "X" && btn3.Text == "X")
+                || (btn4.Text == "X" && btn5.Text == "X" && btn6.Text == "X")
+                || (btn7.Text == "X" && btn8.Text == "X" && btn9.Text == "X")
+                || (btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X")
+                || (btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X")
+                || (btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X")
+                || (btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X")
+                || (btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X"))
+               
+            {
+                turn = 3;
+                lblX.Text = "Winner is X";
+                MessageBox.Show("Winner X");
+                
+            }
+            else if ((btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O")
+               || (btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O")
+               || (btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O")
+               || (btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O")
+               || (btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O")
+               || (btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O")
+               || (btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O")
+               || (btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O"))
+                
+            {
+                turn = 3;
+               lblO.Text = "Winner is O";
+                MessageBox.Show("Winner O");
+                
+            }
+            else if (btn1.Text != "" && btn2.Text != "" && btn3.Text != "" 
+               && btn4.Text != "" && btn5.Text != "" && btn6.Text != ""
+               && btn7.Text != "" && btn8.Text != "" && btn9.Text != "") 
+
+            {
+                turn = 3;
+                MessageBox.Show("No One is Winner");
+            
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+            turn = 0;
+            lblX.Text = "X Player Turn";
+            lblO.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblO.Text = "";
+         
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
